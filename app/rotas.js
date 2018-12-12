0634f7956525002556ada8870a3047b59511b3ad
gilmarf <gilmarf@ecoinf11.lafepe.inet>
2018-12-12 15:56:27 -0300

    Inicio

--- app/rotas.js
+++ app/rotas.js
@@ -6,27 +6,27 @@
             .when('/', {
                 controller: 'HomeController',
                 controllerAs: 'vm',
-                templateUrl: 'paginas/home/index.html'
+                templateUrl: 'public/paginas/home/index.html'
             })
             .when('/empresas', {
                 controller: 'EmpresaListagemController',
                 controllerAs: 'vm',
-                templateUrl: 'paginas/empresa/empresa_listagem.html'
+                templateUrl: 'public/paginas/empresa/empresa_listagem.html'
             })
             .when('/empresas/inclusao', {
                 controller: 'EmpresaInclusaoController',
                 controllerAs: 'vm',
-                templateUrl: 'paginas/empresa/empresa_inclusao.html'
+                templateUrl: 'public/paginas/empresa/empresa_inclusao.html'
             })
             .when('/empresas/alteracao/:id', {
                 controller: 'EmpresaAlteracaoController',
                 controllerAs: 'vm',
-                templateUrl: 'paginas/empresa/empresa_alteracao.html'
+                templateUrl: 'public/paginas/empresa/empresa_alteracao.html'
             })
             .when('/empresas/exclusao/:id', {
                 controller: 'EmpresaExclusaoController',
                 controllerAs: 'vm',
-                templateUrl: 'paginas/empresa/empresa_alteracao.html'
+                templateUrl: 'public/paginas/empresa/empresa_alteracao.html'
             });
     });
 })();
